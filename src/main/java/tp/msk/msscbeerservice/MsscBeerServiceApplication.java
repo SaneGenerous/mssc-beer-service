d98de9e91b74e4362a6f57fee70a401b8bc6b782
Sane Generous <karim2k11@gmail.com>
2024-12-03 18:04:05 +0100

    Inventory service updated to use OpenFeign - closes #36

--- src/main/java/tp/msk/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/tp/msk/msscbeerservice/MsscBeerServiceApplication.java
@@ -3,8 +3,9 @@ package tp.msk.msscbeerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
+@EnableFeignClients
 @SpringBootApplication
 public class MsscBeerServiceApplication {
 
---
+++ src/main/java/tp/msk/msscbeerservice/services/inventory/BeerInventoryServiceFeign.java
@@ -0,0 +1,37 @@
+package tp.msk.msscbeerservice.services.inventory;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import tp.msk.msscbeerservice.services.inventory.model.BeerInventoryDto;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Profile("local-discovery")
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class BeerInventoryServiceFeign implements BeerInventoryService {
+
+    private final InventoryServiceFeignClient inventoryServiceFeignClient;
+
+    @Override
+    public Integer getOnHandInventory(UUID beerId) {
+        log.debug("Calling Inventory Service - BeerId: {}", beerId);
+
+        ResponseEntity<List<BeerInventoryDto>> responseEntity = inventoryServiceFeignClient.getOnHandInventory(beerId);
+
+        Integer onHand = Objects.requireNonNull(responseEntity.getBody())
+                .stream()
+                .mapToInt(BeerInventoryDto::getQuantityOnHand)
+                .sum();
+
+        log.debug("BeerId: {} On Hand: {}", beerId, onHand);
+
+        return onHand;
+    }
+}
