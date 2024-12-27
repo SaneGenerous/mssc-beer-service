99e2dec6d0567d792fa581eef26693ba99072d30
Sane Generous <karim2k11@gmail.com>
2024-12-27 17:54:43 +0100

    HTTP Basic for Feign Client configured - closes #44

--- src/main/java/tp/msk/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
+++ src/main/java/tp/msk/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -5,12 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import tp.msk.msscbeerservice.config.ConfigFeignClient;
 import tp.msk.msscbeerservice.services.inventory.model.BeerInventoryDto;
 
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "inventory-service", fallback = InventoryServiceFeignClientFailover.class)
+@FeignClient(name = "inventory-service", fallback = InventoryServiceFeignClientFailover.class, configuration = ConfigFeignClient.class)
 public interface InventoryServiceFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
